d379bb131898fbc9c3f89516331e62acfd8752f9
Next Level Software Studio <230233445+Lumarysz@users.noreply.github.com>
2026-07-09 13:46:01 +0100

    .

--- organizator.py
+++ organizator.py
@@ -5,31 +5,37 @@ Ele serve para automaticamente organizar as use flags, de um sistema que use o P
 
 
 from pathlib import Path
-import re, sys, os
+import re, sys, os, shutil
 if os.getuid() != 0:
     print("Este script precisa ser executado como root.")
     sys.exit(1)
 
 rule1 = r"^([^./\\, ]+)\/([^./\\, ]+) ([a-zA-Z0-9_+\-]+(?: (?:-[a-zA-Z0-9_+\-]+|[a-zA-Z0-9_\-]+[a-zA-Z0-9_+\-]*))*)$" # LINHA CORRETA
 rule2 = r"^([^./\\, ]+)\/([^./\\, ]+) " #correto sem use flags
-
-
-for j in Path("/etc/portage/package.use").glob("*"):
+dicionário = []
+usedir = Path("/etc/portage/package.use")
+for j in usedir.glob("*"):
     print(f"Processando o ficheiro: {j}")
     with open(j, "r", encoding="utf-8") as f:
         for line in f:
             if not line.strip() or line.strip().startswith("#"):
                 continue  # Ignora linhas vazias ou comentários.
 
-            print(f"Processando a linha: {line}")
-            if re.fullmatch(pattern=rule1, string=line.strip):
-                processado = line.split(" ", maxsplit=1)
-                use = processado[1].strip()
-                classe = processado[0].split("/", maxsplit=1)[0]
-                package = processado[0].split("/", maxsplit=1)[1]
-
             elif re.fullmatch(pattern=rule2, string=line.strip()):
                 print(f'A linha "{line.strip()}", do ficheiro "{j}" não contém flags de USE. Ignorando.')
                 continue  # Ignora linhas sem use flags
-            global FILEPATH
-            FILEPATH = f"/etc/portage/package.use/{classe}/{package}"
+
+            elif re.fullmatch(pattern=rule1, string=line.strip):
+                processado = line.split(" ", maxsplit=1)
+                use = processado[1].strip()
+                package = processado[0]
+                global FILEPATH
+                FILEPATH = Path(f"/etc/portage/package.use/{package}")
+
+                dicionário[package] = use
+
+shutil.rmtree(use)
+usedir.mkdir(parents=True, exist_ok=True)
+
+for k in dicionário:
+    print(k.value())
